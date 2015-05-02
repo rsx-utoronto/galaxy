42bed5d30278ec89335361abae3054a3192e81f0
andrewshao123 <andrewshao123@hotmail.com>
2015-05-02 17:01:38 +0000

    motor function with comments

--- ground_station/motor_control/Arduino_Motor_Microcontroller.ino
+++ ground_station/motor_control/Arduino_Motor_Microcontroller.ino
@@ -1,13 +1,13 @@
-//Range of rotate() function: 31-159
 #include <Servo.h>
-Servo myservo1;
-Servo myservo2;
-float pseudoSpeed1 = -1; //forward+backward
-float pseudoSpeed2 = -0.8; //right+left
-float pseudoSpeed3 = -0.5; //rotate right and left
+Servo myservo1;  //Left wheel
+Servo myservo2;  //Right wheel
+float pseudoSpeed1 = -1; //forward/backward
+float pseudoSpeed2 = -0.8; //right/left
+float pseudoSpeed3 = -0.5; //rotate right/left about centre axis
 
 void setup () {
-  myservo1.attach(6); // Use PWM pin 14 to control Sabertooth.
+  //PWM control wheels
+  myservo1.attach(6);
   myservo2.attach(7);
   Serial.begin(9600);
 }
@@ -20,10 +20,10 @@ void loop(){
 int custom_map(float num){
   int adjustedSpeed;
   
-  if (num>=0){
+  if (num >= 0){
     adjustedSpeed = 69*num+90; 
   }
-  else if (num<=0){
+  else if (num <= 0){
     adjustedSpeed = 59*num+90;
   }
   return adjustedSpeed;
@@ -34,19 +34,19 @@ void wheel_rotate(Servo servo, float joystickSpeed){
 }
 
 void moveRover(){
- if (pseudoSpeed2 == 0 && pseudoSpeed3 == 0){
+ if (pseudoSpeed2 == 0 && pseudoSpeed3 == 0){  //Move Forward/Backward
    wheel_rotate(myservo1, pseudoSpeed1);
    wheel_rotate(myservo2, pseudoSpeed1);
  }
- else if (pseudoSpeed2 > 0 && pseudoSpeed3 == 0){
+ else if (pseudoSpeed2 > 0 && pseudoSpeed3 == 0){  //Right Turn
    wheel_rotate(myservo1, pseudoSpeed1);
-   wheel_rotate(myservo2, pseudoSpeed1*(1-abs(pseudoSpeed2)));   
+   wheel_rotate(myservo2, pseudoSpeed1*(1-abs(pseudoSpeed2))); //Right wheel ranges from 0 to pseudoSpeed1 (determines angle of turn)
  }
- else if (pseudoSpeed2 < 0 && pseudoSpeed3 == 0){
-   wheel_rotate(myservo1, pseudoSpeed1*(1-abs(pseudoSpeed2)));
+ else if (pseudoSpeed2 < 0 && pseudoSpeed3 == 0){  //Left Turn
+   wheel_rotate(myservo1, pseudoSpeed1*(1-abs(pseudoSpeed2))); //Left wheel ranges from 0 to pseudoSpeed1 (determines angle of turn)
    wheel_rotate(myservo2, pseudoSpeed1);
  }
- else if(pseudoSpeed3 != 0){
+ else{ // pseudoSpeed3 != 0; Rotate rover about centre axis
    wheel_rotate(myservo1, pseudoSpeed3);
    wheel_rotate(myservo2, -pseudoSpeed3);
  }
