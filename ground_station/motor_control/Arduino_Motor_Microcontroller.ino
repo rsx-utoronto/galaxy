c4c77c63e3d99c7ce8d26028c214b1fe6f36667d
andrewshao123 <andrewshao123@hotmail.com>
2015-05-02 14:49:18 +0000

    MotorFunction

---
+++ ground_station/motor_control/Arduino_Motor_Microcontroller.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+Servo myservo1;
+Servo myservo2;
+int joystickSpeed = 0;//= Serial.read(whatever come from the joystick speed)
+//probably need to use split function to split the array
+
+void setup () {
+  myservo1.attach(6); // Use PWM pin 14 to control Sabertooth.
+  myservo2.attach(7);
+  Serial.begin(9600);
+}
+
+void loop() {  
+
+}
+
+void sortSpeed(int joystickSpeed){
+  int sortedSpeed = 90+joystickSpeed*89;
+  forward(sortedSpeed); 
+}
+
+void forward(int speed){
+  myservo.write(speed);
+}
