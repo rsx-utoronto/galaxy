e3ccde29569de84b3c363fecdb4a34c4db9293be
andrewshao123 <andrewshao123@hotmail.com>
2015-05-02 16:51:19 +0000

    MotorFunction two wheels

--- ground_station/motor_control/Arduino_Motor_Microcontroller.ino
+++ ground_station/motor_control/Arduino_Motor_Microcontroller.ino
@@ -1,10 +1,10 @@
-//range: 31-159
+//Range of rotate() function: 31-159
 #include <Servo.h>
 Servo myservo1;
 Servo myservo2;
-
-int joystickSpeed = 0;//serial.read (whatever come from the joystick speed)
-float pseudoSpeed = 0; 
+float pseudoSpeed1 = -1; //forward+backward
+float pseudoSpeed2 = -0.8; //right+left
+float pseudoSpeed3 = -0.5; //rotate right and left
 
 void setup () {
   myservo1.attach(6); // Use PWM pin 14 to control Sabertooth.
@@ -13,9 +13,10 @@ void setup () {
 }
 
 void loop(){
-  sortSpeed(pseudoSpeed);
+  moveRover();
 }
 
+//Takes in a number from -1 to 1 and converts to 31 to 159
 int custom_map(float num){
   int adjustedSpeed;
   
@@ -28,10 +29,25 @@ int custom_map(float num){
   return adjustedSpeed;
 }
 
-void sortSpeed(float joystickSpeed){
-  forward(custom_map(joystickSpeed)); 
+void wheel_rotate(Servo servo, float joystickSpeed){
+  servo.write(custom_map(joystickSpeed));
 }
 
-void forward(int speed1){
-  myservo1.write(speed1);
+void moveRover(){
+ if (pseudoSpeed2 == 0 && pseudoSpeed3 == 0){
+   wheel_rotate(myservo1, pseudoSpeed1);
+   wheel_rotate(myservo2, pseudoSpeed1);
+ }
+ else if (pseudoSpeed2 > 0 && pseudoSpeed3 == 0){
+   wheel_rotate(myservo1, pseudoSpeed1);
+   wheel_rotate(myservo2, pseudoSpeed1*(1-abs(pseudoSpeed2)));   
+ }
+ else if (pseudoSpeed2 < 0 && pseudoSpeed3 == 0){
+   wheel_rotate(myservo1, pseudoSpeed1*(1-abs(pseudoSpeed2)));
+   wheel_rotate(myservo2, pseudoSpeed1);
+ }
+ else if(pseudoSpeed3 != 0){
+   wheel_rotate(myservo1, pseudoSpeed3);
+   wheel_rotate(myservo2, -pseudoSpeed3);
+ }
 }
