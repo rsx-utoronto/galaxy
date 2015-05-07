537262bfa2e3dc5ca95212704ddd16c433506169
andrewshao123 <andrewshao123@hotmail.com>
2015-05-07 00:16:10 +0000

    updated sorting data

--- ground_station/RPi_Arduino_Communication/sortingData.ino
+++ ground_station/RPi_Arduino_Communication/sortingData.ino
@@ -1,57 +1,34 @@
-#include String.h
-String string = "1,2,3,4,5,6,7,8,9,10,11.5";
-float newData[11];
-int length = 25;
+String string = "1,2,3,4,5,6,7,8,9.5,10.11"; //Example string to be parsed
+float newData[10]; //We will know the size of the float array
+int start = 0; //start keeps track of next float in string to be parsed
+int data_index = 0; //data_index keeps track of which index to place float in float array
 
 void setup(){
   Serial.begin(9600);
+  Serial.println(string);
+  delay(500);
 }
 
-/*String input = "123,456";
-int firstVal, secondVal;
- 
-for (int i = 0; i < input.length(); i++) {
-  if (input.substring(i, i+1) == ",") {
-    firstVal = input.substring(0, i).toInt();
-    secondVal = input.substring(i+1).toInt();
-    break;
-  }
-}*/
-
 void loop(){
-  Serial.println(string.substring(2,3));
+  //Process string and build float array
+  for(int i = 0; i < string.length(); i++){
+    if(string.substring(i, i+1) == "," || i == string.length()-1){
+      if (string.substring(i, i+1) == ","){ //Check for comma to denote end of float in string
+        newData[data_index] = string.substring(start, i).toFloat();
+      }else{ //Account for last float in string
+        newData[data_index] = string.substring(start, string.length()).toFloat();
+      }
+      start = i+1;
+      data_index++;
+    }
+  }
   
-//  for(int i = 0; i < length; i++){
-//    char c = string[i];
-//    if(c == ','){
-//      continue;
-//    }else{
-//      *newString = c;
-//      if (i < length){
-//        next = string[i+1];
-//        if (next != ','){
-//          *newString = *newString + next;//'1'+0' = "10"
-//        }
-//      }
-//    }
-//  }
+  //Print parsed example float array
+  for(int i = 0; i < 10; i++){
+    Serial.print(newData[i]);
+    Serial.print(" ");
+  }
   
-//  int start = 0;
-//  int data_index = 0;
-//  for(int i = 0; i < length; i++){
-//    if (string.substring(i, i+1) == ",") {
-//      newData[data_index] = string.substring(start, i).toFloat();
-//      start = i+1;
-//      data_index++;
-//    }
-//  }
-
-  char str[] = "0.5";
-  
-  String.toCharArray(str, 10);
-  float pH = atof(str);
-  Serial.println(f);
-  
-    END:
-    goto END;
+  END:
+  goto END;
 }
