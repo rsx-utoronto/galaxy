fa152bf25a1c6d469630055ae7ca91b62420d5f8
RSX <contact@rsx-utoronto.com>
2015-05-10 13:31:30 -0400

    Combined string parse code with motor pwm code

---
+++ rover/mcu/Arduino_Motor_Microcontroller/Arduino_Motor_Microcontroller.ino
@@ -0,0 +1,53 @@
+#include <Servo.h>
+Servo myservo1;  //Left wheel
+Servo myservo2;  //Right wheel
+float pseudoSpeed1 = -1; //forward/backward, joystick element 1
+float pseudoSpeed2 = -0.8; //right/left; joystick element 0
+float pseudoSpeed3 = -0.5; //rotate right/left about centre axis; joystick element 3
+
+void setup () {
+  //PWM control wheels
+  myservo1.attach(6);
+  myservo2.attach(7);
+  Serial.begin(9600);
+}
+
+void loop(){
+  moveRover();
+}
+
+//Takes in a number from -1 to 1 and converts to 31 to 159
+int custom_map(float num){
+  int adjustedSpeed;
+  
+  if (num >= 0){
+    adjustedSpeed = 69*num+90; 
+  }
+  else if (num <= 0){
+    adjustedSpeed = 59*num+90;
+  }
+  return adjustedSpeed;
+}
+
+void wheel_rotate(Servo servo, float joystickSpeed){
+  servo.write(custom_map(joystickSpeed));
+}
+
+void moveRover(){
+ if (pseudoSpeed2 == 0 && pseudoSpeed3 == 0){  //Move Forward/Backward
+   wheel_rotate(myservo1, pseudoSpeed1);
+   wheel_rotate(myservo2, pseudoSpeed1);
+ }
+ else if (pseudoSpeed2 > 0 && pseudoSpeed3 == 0){  //Right Turn
+   wheel_rotate(myservo1, pseudoSpeed1);
+   wheel_rotate(myservo2, pseudoSpeed1*(1-abs(pseudoSpeed2))); //Right wheel ranges from 0 to pseudoSpeed1 (determines angle of turn)
+ }
+ else if (pseudoSpeed2 < 0 && pseudoSpeed3 == 0){  //Left Turn
+   wheel_rotate(myservo1, pseudoSpeed1*(1-abs(pseudoSpeed2))); //Left wheel ranges from 0 to pseudoSpeed1 (determines angle of turn)
+   wheel_rotate(myservo2, pseudoSpeed1);
+ }
+ else{ // pseudoSpeed3 != 0; Rotate rover about centre axis
+   wheel_rotate(myservo1, pseudoSpeed3);
+   wheel_rotate(myservo2, -pseudoSpeed3);
+ }
+}
---
+++ rover/mcu/May_10_Motor_MCU/May_10_Motor_MCU.ino
@@ -0,0 +1,95 @@
+#include <Servo.h>
+
+float newData[200]; //We will know the size of the float array
+
+int start = 0; //start keeps track of next float in string to be parsed
+int data_index = 0; //data_index keeps track of which index to place float in float array
+
+float verticalSpeed = -1; //forward/backward, joystick element 1
+float horizontalSpeed = -0.8; //right/left; joystick element 0
+float rotationSpeed = -0.5; //rotate right/left about centre axis; joystick element 3
+
+Servo myservo1;  //Left wheel
+Servo myservo2;  //Right wheel
+
+void setup(){
+  //PWM control wheels
+  myservo1.attach(6);
+  myservo2.attach(7);
+  
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  delay(500);
+}
+
+void loop(){
+  
+  //Process string and build float array
+  if(Serial2.available() > 0){
+
+    String string = Serial2.readStringUntil('\n');
+    Serial.println(string);
+
+    for(int i = 0; i < string.length(); i++){
+      if(string.substring(i, i+1) == "," || i == string.length()-1){
+        if (string.substring(i, i+1) == ","){ //Check for comma to denote end of float in string
+          newData[data_index] = string.substring(start, string.length()).toFloat();
+        }
+        else{ //Account for last float in string
+          newData[data_index] = string.substring(start, string.length()).toFloat();
+        }
+        start = i+1;
+        data_index++;
+      }
+    }
+    
+  verticalSpeed = newData[1];
+  horizontalSpeed = newData[0];
+  rotationSpeed = newData[3];
+  
+  Serial.print("Vertical Speed: ");
+  Serial.println(verticalSpeed);
+  Serial.print("Horizontal Speed: ");
+  Serial.println(horizontalSpeed);
+  Serial.print("Rotation Speed: ");
+  Serial.println(rotationSpeed);
+  
+  moveRover(verticalSpeed, horizontalSpeed, rotationSpeed);
+  }
+}
+
+//Takes in a number from -1 to 1 and converts to 31 to 159
+int custom_map(float num){
+  int adjustedSpeed;
+  
+  if (num >= 0){
+    adjustedSpeed = 69*num+90; 
+  }
+  else if (num <= 0){
+    adjustedSpeed = 59*num+90;
+  }
+  return adjustedSpeed;
+}
+
+void wheel_rotate(Servo servo, float joystickSpeed){
+  servo.write(custom_map(joystickSpeed));
+}
+
+void moveRover(float verticalSpeed, float horizontalSpeed, float rotationSpeed){
+  if (horizontalSpeed == 0 && rotationSpeed == 0){  //Move Forward/Backward
+    wheel_rotate(myservo1, verticalSpeed);
+    wheel_rotate(myservo2, verticalSpeed);
+  }
+  else if (horizontalSpeed > 0 && rotationSpeed == 0){  //Right Turn
+    wheel_rotate(myservo1, verticalSpeed);
+    wheel_rotate(myservo2, verticalSpeed*(1-abs(horizontalSpeed))); //Right wheel ranges from 0 to verticalSpeed (determines angle of turn)
+  }
+  else if (horizontalSpeed < 0 && rotationSpeed == 0){  //Left Turn
+    wheel_rotate(myservo1, verticalSpeed*(1-abs(horizontalSpeed))); //Left wheel ranges from 0 to verticalSpeed (determines angle of turn)
+    wheel_rotate(myservo2, verticalSpeed);
+  }
+  else{ // rotationSpeed != 0; Rotate rover about centre axis
+    wheel_rotate(myservo1, rotationSpeed);
+    wheel_rotate(myservo2, -rotationSpeed);
+  }
+}
